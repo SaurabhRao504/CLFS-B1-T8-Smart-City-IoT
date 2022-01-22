cbd56988f4cd266d13d53daed2302bce81c3f404
SaurabhRao504 <65291247+SaurabhRao504@users.noreply.github.com>
2022-01-22 12:01:47 +0530

    Create smoke_mail.sh

---
+++ smoke_mail.sh
@@ -0,0 +1,28 @@
+present_date=$(date +"%m-%d-%y")
+echo $present_date
+
+present_time=$(date +"%T")
+echo $present_time
+
+smoke_detect_mail () {
+message2=$(cat <<EOF
+{
+   "Subject": {
+       "Data": "from smoke detecting system",
+       "Charset": "UTF-8"
+   },
+   "Body": {
+       "Text": {
+           "Data": "smoke detected Date is $1, Time is $2",
+           "Charset": "UTF-8"
+       }
+   }
+}
+EOF
+)
+
+echo "$message2" > smoke_info.json
+
+aws ses send-email --from "saurabh.rao2000@gmail.com" --destination "ToAddresses=saurabh.rao2000@gmail.com" --region ap-south-1 --profile default --message file://smoke_info.json
+
+}
